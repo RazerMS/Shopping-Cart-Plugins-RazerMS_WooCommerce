76c784b623b8559f06beeb2592422e8e1a64a6fa
Syamimi Aziah <syamimi.molpay@gmail.com>
2017-09-21 16:50:33 +0800

    Update wc-molpay.php

--- woocommerce-molpay/wc-molpay.php
+++ woocommerce-molpay/wc-molpay.php
@@ -95,6 +95,7 @@ function wcmolpay_gateway_load() {
             $this->description = $this->settings['description'];
             $this->merchant_id = $this->settings['merchant_id'];
             $this->verify_key = $this->settings['verify_key'];
+	    $this->secret_key = $this->settings['secret_key'];
 
             // Actions.
             add_action( 'valid_molpay_request_returnurl', array( &$this, 'check_molpay_response_returnurl' ) );
@@ -113,6 +114,8 @@ function wcmolpay_gateway_load() {
 
             // Checking if verify_key is not empty.
             $this->verify_key == '' ? add_action( 'admin_notices', array( &$this, 'verify_key_missing_message' ) ) : '';
+		
+	    $this->secret_key == '' ? add_action( 'admin_notices', array( &$this, 'secret_key_missing_message' ) ) : '';
         }
 
         /**
@@ -170,13 +173,19 @@ function wcmolpay_gateway_load() {
                 'merchant_id' => array(
                     'title' => __( 'Merchant ID', 'wcmolpay' ),
                     'type' => 'text',
-                    'description' => __( 'Please enter your MOLPay Merchant ID.', 'wcmolpay' ) . ' ' . sprintf( __( 'You can to get this information in: %sMOLPay Account%s.', 'wcmolpay' ), '<a href="https://www.onlinepayment.com.my/MOLPay/" target="_blank">', '</a>' ),
+                    'description' => __( 'Please enter your MOLPay Merchant ID.', 'wcmolpay' ) . ' ' . sprintf( __( 'You can to get this information in: %sMOLPay Account%s.', 'wcmolpay' ), '<a href="https://portal.molpay.com/" target="_blank">', '</a>' ),
                     'default' => ''
                 ),
                 'verify_key' => array(
                     'title' => __( 'Verify Key', 'wcmolpay' ),
                     'type' => 'text',
-                    'description' => __( 'Please enter your MOLPay Verify Key.', 'wcmolpay' ) . ' ' . sprintf( __( 'You can to get this information in: %sMOLPay Account%s.', 'wcmolpay' ), '<a href="https://www.onlinepayment.com.my/MOLPay/" target="_blank">', '</a>' ),
+                    'description' => __( 'Please enter your MOLPay Verify Key.', 'wcmolpay' ) . ' ' . sprintf( __( 'You can to get this information in: %sMOLPay Account%s.', 'wcmolpay' ), '<a href="https://portal.molpay.com/" target="_blank">', '</a>' ),
+                    'default' => ''
+                ),
+                'secret_key' => array(
+                    'title' => __( 'Secret Key', 'wcmolpay' ),
+                    'type' => 'text',
+                    'description' => __( 'Please enter your MOLPay Secret Key.', 'wcmolpay' ) . ' ' . sprintf( __( 'You can to get this information in: %sMOLPay Account%s.', 'wcmolpay' ), '<a href="https://portal.molpay.com/" target="_blank">', '</a>' ),
                     'default' => ''
                 )
             );
@@ -305,7 +314,7 @@ function wcmolpay_gateway_load() {
             $paydate = $_POST['paydate'];
             $channel = $_POST['channel'];
             $skey = $_POST['skey'];
-            $vkey = $this->verify_key;
+            $vkey = $this->secret_key;
 			
 			while ( list($k,$v) = each($_POST) ) {
 			$postData[]= $k."=".$v;
@@ -380,7 +389,7 @@ function wcmolpay_gateway_load() {
             $appcode = $_POST['appcode'];
             $paydate = $_POST['paydate'];
             $skey = $_POST['skey'];
-            $vkey = $this->verify_key;
+            $vkey = $this->secret_key;
 			
 			while ( list($k,$v) = each($_POST) ) {
 			$postData[]= $k."=".$v;
@@ -444,7 +453,7 @@ function wcmolpay_gateway_load() {
             $appcode = $_POST['appcode'];
             $paydate = $_POST['paydate'];
             $skey = $_POST['skey'];
-            $vkey = $this->verify_key;
+            $vkey = $this->secret_key;
 			
             $key0 = md5($tranID.$orderid.$status.$domain.$amount.$currency);
             $key1 = md5($paydate.$domain.$key0.$appcode.$vkey);
@@ -496,6 +505,13 @@ function wcmolpay_gateway_load() {
         public function verify_key_missing_message() {
             $message = '<div class="error">';
             $message .= '<p>' . sprintf( __( '<strong>Gateway Disabled</strong> You should inform your Verify Key in MOLPay. %sClick here to configure!%s' , 'wcmolpay' ), '<a href="' . get_admin_url() . 'admin.php?page=wc-settings&tab=checkout&section=wc_molpay_gateway">', '</a>' ) . '</p>';
+            $message .= '</div>';
+            echo $message;
+        }
+	    
+	public function secret_key_missing_message() {
+            $message = '<div class="error">';
+            $message .= '<p>' . sprintf( __( '<strong>Gateway Disabled</strong> You should fill in your Verify Key in MOLPay. %sClick here to configure!%s' , 'wcmolpay' ), '<a href="' . get_admin_url() . 'admin.php?page=wc-settings&tab=checkout&section=wc_molpay_gateway">', '</a>' ) . '</p>';
             $message .= '</div>';
             echo $message;
         }
